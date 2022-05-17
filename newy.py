7fb656a2238c92794273b76320fe3067b73d9932
Jovan Ford <Jovan.ford14@gmial.com>
2022-05-17 14:13:18 -0400

    changed .

---
+++ newy.py
@@ -0,0 +1,25 @@
+#comment 
+# another comment 
+x = 5 #int 
+b = 3.0
+l = ["Jovan", "James", "Elijah", "RC", "David"]
+#Regular List
+print(l)
+#list type
+print(type(l))
+#list size
+print(len(l))
+#Sorted List
+l.sort()
+print(l)
+#Looping through list 
+for x in l:
+    print(x)
+    if x == "Elijah":
+        break
+#Looping through String
+t = "tommy"
+for x in t:
+    print (x)
+    if x == 'm':
+        break
